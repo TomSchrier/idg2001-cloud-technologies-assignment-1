b0f5f44a6f34f0eaf5dbcedfc1b843edf2e32e8d
Tom <54577949+TomSchrier@users.noreply.github.com>
2021-03-11 21:18:48 +0100

    connectet Gerardos code to MongoDB Atlas

--- controllers/customerControllers.js
+++ controllers/customerControllers.js
@@ -6,6 +6,7 @@ const Customer = require('../models/customerModel');
     res.json({ message: "POST createNewcustomer" }); // dummy function for now
 };*/
 
+//POST create new customer
 const createNewCustomer = (req, res) => {
 
     const newCustomer = new Customer({
@@ -24,16 +25,74 @@ const createNewCustomer = (req, res) => {
         .catch((error) => res.status(500).json(`–ERROR: There was an error adding the user ${req.query.first_name} to the database.`));
 };
 
+//GET get customer by personal number
+/*
 const getCustomerByPersonalNumber = (req, res) => {
     res.json({ message: "GET getCustomer" });
-}
+}*/
 
-const deleteCustomerByPersonalNumber = (req, res) => {
-    res.json({ message: "DELETE customer" });
+const getCustomerByPersonalNumber = (req, res) => {
+    let personalNumberToFind = parseInt(req.query.personal_number);
+
+    if (personalNumberToFind) {
+        //find by personal_number
+        Customer.find({ personal_number: personalNumberToFind })
+            .then(customers => { res.status(200).json(customers) })
+            .catch((error) => { res.status(500).json(error) });
+    };
 };
 
+/*
+const deleteCustomerByPersonalNumber = (req, res) => {
+    res.json({ message: "DELETE customer" });
+};*/
+
+const deleteCustomerByPersonalNumber = (req, res) => {
+    let personalNumberToDelete = parseInt(req.query.personal_number);
+
+    if (!personalNumberToDelete) {
+        res.status(400).json("You need to specify a personal number in the query")
+    } else {
+        Customer.findOneAndDelete({ personal_number: personalNumberToDelete })
+            .then(() => res.status(200).json(`The customer with the personal number ${personalNumberToDelete} has been removed from the database`))
+            .catch((error) => res.status(500).json(error));
+    }
+};
+
+/*
 const updateCustomerDetails = (req, res) => {
     res.json({ message: "UPDATE UpdateCustomerDetails" });
+};*/
+
+const updateCustomerDetails = (req, res) => {
+    let id = req.query.id;
+    let newAccountNumber = parseInt(req.query.account_number);
+    /*let newFirstName = req.query.first_name;
+    let newLastName = req.query.last_name;
+    let newDateOfBirth = req.query.date_of_birth;
+    let newCity = req.query.city;*/
+
+    if (!id) {
+        res.status(400).json("You need to provide a id");
+    } else {
+        if (newAccountNumber) {
+            Customer.findOneAndUpdate({ id: id }, { account_number: newAccountNumber })
+                .then(() => res.status(200).json(`The status of acc with the id ${id} was updated to ${newStatus}`))
+                .catch((error) => res.status(500).json(error));
+        };
+
+        /*if (newPlace) {
+            Student.findOneAndUpdate({ userName: userNameToFind }, { place: newPlace })
+                .then(() => res.status(200).json(`The place of ${userNameToFind} was updated to ${newPlace}`))
+                .catch((error) => res.status(500).json(error));
+        };
+
+        if (newUserName) {
+            Student.findOneAndUpdate({ userName: userNameToFind }, { userName: newUserName })
+                .then(() => res.status(200).json(`The name of ${userNameToFind} was updated to ${newUserName}`))
+                .catch((error) => res.status(500).json(error));
+        }; */
+    };
 };
 
 //export functions so we can import it to our routes/customerRoutes.js
