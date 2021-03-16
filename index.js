44403c1a6f8a696bca718ffa4b9082b5aed22bc1
Tom <54577949+TomSchrier@users.noreply.github.com>
2021-03-16 15:10:30 +0100

    prepare heroku

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ Not to be confused with index.ejs - this is the main file.
 We set up some routes that renders each page, require all 
 the packages and start the server
 */
-
+require('dotenv').config();
+const helmet = require('helmet');
+const compression = require('compression');
 const express = require("express");
 const app = express();
 const routes = require('./routes/customerRoutes'); // import the routes
@@ -12,11 +14,13 @@ const mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+app.use(helmet());
 app.set('view engine', 'ejs');
 app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.resolve(__dirname, "views"));
 app.use(express.static('public'));
+app.use(compression()); //Compress all routes
 
 // Use static server to serve the ABC Bank Website
 app.get(
@@ -46,19 +50,23 @@ app.get(
 app.use('/', routes); //to use the imported routes
 
 mongoose.connect(
-    'mongodb+srv://tomschr:Cidc6riccj5vinT@cluster0.krk93.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    process.env.MONGODB_URI,
     {
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+      replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
     },
-    (err) => {
-        if (err) return console.log("Error: ", err);
-        console.log("MongoDB Connection -- Ready state is:", mongoose.connection.readyState);
+    function (err) {
+      if (err) return console.log("Error: ", err);
+      console.log(
+        "MongoDB Connection -- Ready state is:",
+        mongoose.connection.readyState
+      );
     }
-);
-
+  );
+  
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('App is listening on port ' + listener.address().port)
 })
